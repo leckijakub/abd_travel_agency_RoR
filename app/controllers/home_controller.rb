4a5bedcdea047619a8791a6d7f3442afd63aef66
Jakub Lecki <lec.jakub@gmail.com>
2021-12-15 18:06:10 +0100

    app: controllrs: home_controller: update resetting the databaase method
    Signed-off-by: Jakub Lecki <lec.jakub@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,25 +17,20 @@ class HomeController < ApplicationController
   end
 
   def self.test_dell_all
-    puts "\n\nDebug-msg: deleting old offers\n\n"
-    @offers = Offer.all
-    @offers.each do |offer|
-      offer.destroy
-    end
-    
     puts "\n\nDebug-msg: deleting old users\n\n"
-    @users = User.all
-    @users.each do |user|
-      user.destroy
-    end
+    Client.destroy_all
+    Employee.destroy_all
+    User.destroy_all
+    puts "\n\nDebug-msg: deleting old offers\n\n"
+    Offer.destroy_all
+    puts "\n\nDebug-msg: deleting old reservations\n\n"
+    Reservation.destroy_all
   end
 
   def self.test_add_users
     puts "\n\nDebug-msg: adding new users\n\n"
-    # User.create!(email: "a@a", password: "1234563", name: "Jan", surname: "Daczyński", uid: "u01")
     Client.create!(email: "a@a", password: "a", name: "Jan", surname: "Daczyński", uid: "u01", address: "tam", phone_number: "666 666 666")
     Employee.create!(email: "b@b", password: "a", name: "Edward", surname: "Gdaczek", uid: "u02", position: "admin")
-    # Client.create!(address: "tam", phone_number:"666 666 666")
   end
 
 end
